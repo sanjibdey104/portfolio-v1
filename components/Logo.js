d0007a615b4c19f59128c4beb6777f22f6998837
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-06-14 11:22:54 +0530

    Style: change logo style

--- components/Logo.js
+++ components/Logo.js
@@ -2,41 +2,25 @@ import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledLogo = styled.h2`
+const StyledLogo = styled.h3`
+
+    font-size: calc(0.85rem, 2vw);
     cursor: pointer;
-    font-size: 2.5rem;
-    background-color: ${({theme}) => theme.backgroundColor};
-    color: ${({ theme }) => theme.primary};
     margin-right: auto;
+    padding: 0.5rem;
+
+    color: ${({ theme }) => theme.textColor};
+    border: 2px solid ${({theme}) => theme.textColor};
+    border-bottom-right-radius: 50%;
 
     display: grid;
     place-content: center;
-
-    border-radius: 50%;
-    height: 3.2rem;
-    width: 3.2rem;
-    position: relative;
-    overflow: hidden;
-    box-shadow: var(--box-shadow);
-
-    &::after {
-        content: "";
-        height: 0.9rem;
-        width: 0.9rem;
-        border-radius: 50%;
-        background-color: ${({theme}) => theme.primary};
-
-        position: absolute;
-        top: 0;
-        right: 0;
-    }
-
 `
 
 const Logo = () => {
     return (
         <Link href="/">
-            <StyledLogo>S</StyledLogo>
+            <StyledLogo>SKD</StyledLogo>
         </Link>
     )
 }
