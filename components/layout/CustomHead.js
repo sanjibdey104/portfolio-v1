d62dc3103eafaa74265df080516ea85dd33f038c
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-19 15:22:32 +0530

    refactor: preconnect google fonts

--- components/layout/CustomHead.js
+++ components/layout/CustomHead.js
@@ -7,6 +7,8 @@ const CustomHead = () => {
       <title>Sanjib Kumar Dey - Web Developer</title>
       <meta name="description" content="Personal Portfolio Website" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap"
         rel="stylesheet"
