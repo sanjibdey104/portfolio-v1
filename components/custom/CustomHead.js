17178f1fccd056c62215df2628b8296b8f53383e
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-09-02 18:27:09 +0530

    update favicons

--- components/custom/CustomHead.js
+++ components/custom/CustomHead.js
@@ -6,7 +6,24 @@ const CustomHead = () => {
     <Head>
       <title>Sanjib Kumar Dey - Web Developer</title>
       <meta name="description" content="Personal Portfolio Website" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicons/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
         rel="preconnect"
