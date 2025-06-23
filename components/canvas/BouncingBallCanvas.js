ea868d4c6cedecaa2dbc692b2ced2f3559cfad78
sanjib <sanjibdey.dey4@gmail.com>
2025-06-23 21:20:29 +0530

    update: separate bouncing ball canvas

---
+++ components/canvas/BouncingBallCanvas.js
@@ -0,0 +1,66 @@
+import { useEffect } from "react";
+
+const BouncingBallCanvas = () => {
+  useEffect(() => {
+    const canvas = document.getElementById("bouncing-ball-canvas");
+    const ctx = canvas.getContext("2d");
+
+    if (canvas.getContext) {
+      const gravity = 0.4;
+      let hasBottomCollision = false;
+
+      const ball = {
+        x: 44,
+        y: 0,
+        radius: 3,
+        yVelocity: 0,
+      };
+
+      function renderBall(updatedX, updatedY) {
+        // render the ball
+        ctx.beginPath();
+        // x, y, radius, startAngle, endAngle
+        ctx.arc(updatedX, updatedY, ball.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.fillStyle = "red";
+      }
+
+      function dropBall() {
+        // clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // fall calculation
+        ball.yVelocity += gravity; // Gravity increases the velocity (faster fall)
+        ball.y += ball.yVelocity; // That velocity is applied to the position (ball moves)
+
+        hasBottomCollision = ball.y + ball.radius >= canvas.height;
+
+        if (hasBottomCollision) {
+          ball.y = canvas.height - ball.radius;
+          ball.yVelocity *= -0.8;
+        }
+
+        renderBall(ball.x, ball.y);
+
+        requestAnimationFrame(dropBall);
+      }
+
+      dropBall();
+    }
+  }, []);
+
+  return (
+    <canvas
+      id="bouncing-ball-canvas"
+      width="80"
+      height="31"
+      style={{
+        width: "80px",
+        height: "31px",
+        zIndex: "-1",
+      }}
+    ></canvas>
+  );
+};
+
+export default BouncingBallCanvas;
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import Logo from "./Logo";
 import styled from "styled-components";
+import Logo from "./Logo";
+import BouncingBallCanvas from "../canvas/BouncingBallCanvas";
 
 const StyledHeader = styled.header`
   width: 100%;
@@ -10,7 +10,7 @@ const StyledHeader = styled.header`
   display: flex;
   align-items: center;
 
-  #bounce {
+  #bouncing-ball-canvas {
     position: absolute;
     left: 0;
     bottom: 0;
@@ -19,68 +19,11 @@ const StyledHeader = styled.header`
 `;
 
 const Header = () => {
-  useEffect(() => {
-    const canvas = document.getElementById("bounce");
-    const ctx = canvas.getContext("2d");
-
-    if (canvas.getContext) {
-      const gravity = 0.4;
-      let hasBottomCollision = false;
-
-      const ball = {
-        x: 44,
-        y: 0,
-        radius: 3,
-        yVelocity: 0,
-      };
-
-      function renderBall(updatedX, updatedY) {
-        // render the ball
-        ctx.beginPath();
-        // x, y, radius, startAngle, endAngle
-        ctx.arc(updatedX, updatedY, ball.radius, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.fillStyle = "red";
-      }
-
-      function dropBall() {
-        // clear the canvas
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // fall calculation
-        ball.yVelocity += gravity; // Gravity increases the velocity (faster fall)
-        ball.y += ball.yVelocity; // That velocity is applied to the position (ball moves)
-
-        hasBottomCollision = ball.y + ball.radius >= canvas.height;
-
-        if (hasBottomCollision) {
-          ball.y = canvas.height - ball.radius;
-          ball.yVelocity *= -0.8;
-        }
-
-        renderBall(ball.x, ball.y);
-
-        requestAnimationFrame(dropBall);
-      }
-
-      dropBall();
-    }
-  }, []);
-
   return (
     <StyledHeader>
       <Logo />
 
-      <canvas
-        id="bounce"
-        width="80"
-        height="31"
-        style={{
-          width: "80px",
-          height: "31px",
-          zIndex: "-1",
-        }}
-      ></canvas>
+      <BouncingBallCanvas />
     </StyledHeader>
   );
 };
